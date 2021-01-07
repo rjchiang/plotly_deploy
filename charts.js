23d223b46a2529454fa86ef0e955a58eb2a56cea
rjchiang <rayjay.chiang@pepperdine.edu>
2021-01-06 17:18:59 -0800

    backticks

--- charts.js
+++ charts.js
@@ -82,7 +82,7 @@ function buildCharts(sample) {
     // Hint: Get the the top 10 otu_ids and map them in descending order  
     //  so the otu_ids with the most bacteria are last. 
 
-    var yticks = otuIds.slice(0,10).map(otuId => 'OTU ${otuId}').reverse();
+    var yticks = otuIds.slice(0,10).map(otuId => `OTU ${otuId}`).reverse();
 
     // 8. Create the trace for the bar chart. 
     var barData = [
